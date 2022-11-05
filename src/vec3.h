72b994ecadfb934e95a8493072d01cc43ebbfb17
yielduck <yielduck@gmail.com>
2022-11-05 18:17:30 +0300

    vec3: component-wise multiplication and division

--- src/vec3.h
+++ src/vec3.h
@@ -37,6 +37,15 @@ inline vec3 operator*(vec3 const &v, float const f)
         v.z * f,
     };
 }
+inline vec3 operator*(vec3 const &v1, vec3 const &v2)
+{
+    return
+    {
+        v1.x * v2.x,
+        v1.y * v2.y,
+        v1.z * v2.z,
+    };
+}
 inline vec3 operator/(vec3 const &v, float const f)
 {
     return
@@ -44,6 +53,15 @@ inline vec3 operator/(vec3 const &v, float const f)
         v.x / f,
         v.y / f,
         v.z / f,
+    };
+}
+inline vec3 operator/(vec3 const &v1, vec3 const &v2)
+{
+    return
+    {
+        v1.x / v2.x,
+        v1.y / v2.y,
+        v1.z / v2.z,
     };
 }
 inline float dot(vec3 const &v1, vec3 const &v2)
